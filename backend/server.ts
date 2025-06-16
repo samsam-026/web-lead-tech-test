8ee31b996ceb87a60e9381134acd9d49c9076705
Pedro Roque <pmm26@protonmail.com>
2025-06-16 17:13:20 +0100

    server changes

--- backend/server.ts
+++ backend/server.ts
@@ -1,12 +1,20 @@
 import express, { Request, Response } from "express";
 import http from "http";
 import { Server, Socket } from "socket.io";
+import cors from "cors";
+import bodyParser from "body-parser";
+
 
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: { origin: "*" },
 });
+
+app.use(cors({ origin: "*" }));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
 
 // Start the server on port 3000
 const PORT = process.env.PORT || 3001;
